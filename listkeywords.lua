9cfd858ca93c707227bab9613b1085d92c880725
Michal Hoftich <michal.h21@gmail.com>
2018-03-07 14:22:24 +0100

    změn interface

--- listkeywords.lua
+++ listkeywords.lua
@@ -1,9 +1,9 @@
 local risparser = require "risparser"
-local test = risparser.new()
-test:parse_file()
+-- local test = risparser.new()
+risparser:parse_file()
 
 local keywords = {}
-for k, v in ipairs(test.records) do
+for k, v in ipairs(risparser:get_records()) do
   for _, kw in ipairs(v.KW or {}) do
     local current = keywords[kw] or {}
     table.insert(current, k)
